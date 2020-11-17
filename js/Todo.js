3f6d0d124cd6afd4fcd7166f868d3e3409d21b8f
rose6948 <pyhan23@gmail.com>
2020-11-17 17:55:08 +0900

    11/17 fix

--- js/Todo.js
+++ js/Todo.js
@@ -1,8 +1,7 @@
+  
 const Todoform = document.querySelector(".js-TodoForm"),
-        TodoInput = Todoform.querySelectorAll("input"),
-        // TodoDate = Todoform.querySelector(".input"),
-        // TodoTime = Todoform.querySelector("..input"),
-        TodoList = document.querySelector(".js-TodoList");
+TodoInput = Todoform.querySelector("input"),
+TodoList = document.querySelector(".js-TodoList");
 
 const  TODO_LS = "Todos";
 let TodoArr = [];
@@ -10,70 +9,66 @@ let TodoArr = [];
 
 function DeleteTodo(event)
 {
-    const btn = event.target;
-    const li = btn.parentNode;
-    TodoList.removeChild(li);
-    const CleanTodos = TodoArr.filter(function(Todo){
-        return Todo.id !== parseInt(li.id);
-    });
-    TodoArr = CleanTodos;
-    SaveTodos();
+const btn = event.target;
+const li = btn.parentNode;
+TodoList.removeChild(li);
+const CleanTodos = TodoArr.filter(function(Todo){
+return Todo.id !== parseInt(li.id);
+});
+TodoArr = CleanTodos;
+SaveTodos();
 }
 
 function SaveTodos()
 { 
-    localStorage.setItem(TODO_LS, JSON.stringify(TodoArr));
+localStorage.setItem(TODO_LS, JSON.stringify(TodoArr));
 }
 
 function PaintTodo(text)
 {
-    const li = document.createElement("li");
+const li = document.createElement("li");
+const delBtn = document.createElement("button");
+delBtn.innerText ="❌";
+delBtn.addEventListener("click", DeleteTodo);
+const span = document.createElement("span");
+span.innerText = text;
+const newId = TodoArr.length + 1;
 
-    const delBtn = document.createElement("button");
-    delBtn.innerText ="❌";
-    delBtn.addEventListener("click", DeleteTodo);
-    
-    const span = document.createElement("span");
-    span.innerText = text;
-    const newId = TodoArr.length + 1;
-    
-    li.appendChild(delBtn);
-    li.appendChild(span);
-    li.id  = newId;
-    TodoList.appendChild(li);
-    const TodoObj = {
-        text : text,
-        id : newId,
-        };
-        console.log(TodoObj);
-        TodoArr.push(TodoObj);
-        SaveTodos()
+li.appendChild(delBtn);
+li.appendChild(span);
+li.id  = newId;
+TodoList.appendChild(li);
+const TodoObj = {
+text : text,
+id : newId
+};
+TodoArr.push(TodoObj);
+SaveTodos()
 }
 
 function HandleSubmit(event)
 {
-    event.preventDefault();
-    const currentVlaue = TodoInput.value;
-    
-    PaintTodo(currentVlaue);
-    TodoInput.value ="";
+event.preventDefault();
+const currentVlaue = TodoInput.value;
+PaintTodo(currentVlaue);
+TodoInput.value ="";
 }
 
 function loadTodos()
 {
-    const LoadedTodos = localStorage.getItem(TODO_LS);
-    if(LoadedTodos !== null)
-    {
-       const parsedTodos = JSON.parse(LoadedTodos);
-        parsedTodos.forEach(function(Todo){
-            PaintTodo(Todo.text);
-        });
-    }
+const LoadedTodos = localStorage.getItem(TODO_LS);
+if(LoadedTodos !== null)
+{
+const parsedTodos = JSON.parse(LoadedTodos);
+parsedTodos.forEach(function(Todo){
+    PaintTodo(Todo.text);
+});
+}
 }
 
 function init()
 {
-    loadTodos();
-    Todoform.addEventListener("submit", HandleSubmit);
+loadTodos();
+Todoform.addEventListener("submit", HandleSubmit);
 }
 init();
